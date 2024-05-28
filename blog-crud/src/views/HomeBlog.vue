0ae048880d0dae36beaa5c7c93c401dcb7389516
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-05-28 21:44:00 +0200

    feat: add pages

---
+++ blog-crud/src/views/HomeBlog.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+import { getBlogs } from '@/services/blogsApi'
+import HeaderBlog from '@/components/HeaderBlog.vue'
+
+const GridBlogs = defineAsyncComponent(() => import('@/components/GridBlogs.vue'))
+
+const blogs = await getBlogs()
+</script>
+<template>
+  <main class="main-content">
+    <HeaderBlog />
+    <GridBlogs :blogs="blogs"></GridBlogs>
+  </main>
+</template>
+
+<style scoped>
+.main-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  padding: 2rem 8rem;
+}
+</style>
