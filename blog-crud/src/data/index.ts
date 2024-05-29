f2cf401c7791ac2c788c6c0147b8f7b9a729cb2c
Rodrigo Herranz <rodrigoasime@gmail.com>
2024-05-29 20:45:51 +0200

    chore: update api

--- blog-crud/src/data/index.ts
+++ blog-crud/src/data/index.ts
@@ -2,141 +2,141 @@ export default [
   {
     title: 'blog title 1',
     publish: '2021-01-01',
-    resume: 'blog resume 1',
-    author: 'blog author 1',
-    content: 'blog content 1'
+    resume: 'resume 1',
+    author: 'author 1',
+    content: 'content 1'
   },
   {
     title: 'blog title 2',
     publish: '2021-01-02',
-    resume: 'blog resume 2',
-    author: 'blog author 2',
-    content: 'blog content 2'
+    resume: 'resume 2',
+    author: 'author 2',
+    content: 'content 2'
   },
   {
     title: 'blog title 3',
     publish: '2021-01-03',
-    resume: 'blog resume 3',
-    author: 'blog author 3',
-    content: 'blog content 3'
+    resume: 'resume 3',
+    author: 'author 3',
+    content: 'content 3'
   },
   {
     title: 'blog title 4',
     publish: '2021-01-04',
-    resume: 'blog resume 4',
-    author: 'blog author 4',
-    content: 'blog content 4'
+    resume: 'resume 4',
+    author: 'author 4',
+    content: 'content 4'
   },
   {
     title: 'blog title 5',
     publish: '2021-01-05',
-    resume: 'blog resume 5',
-    author: 'blog author 5',
-    content: 'blog content 5'
+    resume: 'resume 5',
+    author: 'author 5',
+    content: 'content 5'
   },
   {
     title: 'blog title 6',
     publish: '2021-01-06',
-    resume: 'blog resume 6',
-    author: 'blog author 6',
-    content: 'blog content 6'
+    resume: 'resume 6',
+    author: 'author 6',
+    content: 'content 6'
   },
   {
     title: 'blog title 7',
     publish: '2021-01-07',
-    resume: 'blog resume 7',
-    author: 'blog author 7',
-    content: 'blog content 7'
+    resume: 'resume 7',
+    author: 'author 7',
+    content: 'content 7'
   },
   {
     title: 'blog title 8',
     publish: '2021-01-08',
-    resume: 'blog resume 8',
-    author: 'blog author 8',
-    content: 'blog content 8'
+    resume: 'resume 8',
+    author: 'author 8',
+    content: 'content 8'
   },
   {
     title: 'blog title 9',
     publish: '2021-01-09',
-    resume: 'blog resume 9',
-    author: 'blog author 9',
-    content: 'blog content 9'
+    resume: 'resume 9',
+    author: 'author 9',
+    content: 'content 9'
   },
   {
     title: 'blog title 10',
     publish: '2021-01-10',
-    resume: 'blog resume 10',
-    author: 'blog author 10',
-    content: 'blog content 10'
+    resume: 'resume 10',
+    author: 'author 10',
+    content: 'content 10'
   },
   {
     title: 'blog title 11',
     publish: '2021-01-11',
-    resume: 'blog resume 11',
-    author: 'blog author 11',
-    content: 'blog content 11'
+    resume: 'resume 11',
+    author: 'author 11',
+    content: 'content 11'
   },
   {
     title: 'blog title 12',
     publish: '2021-01-12',
-    resume: 'blog resume 12',
-    author: 'blog author 12',
-    content: 'blog content 12'
+    resume: 'resume 12',
+    author: 'author 12',
+    content: 'content 12'
   },
   {
     title: 'blog title 13',
     publish: '2021-01-13',
-    resume: 'blog resume 13',
-    author: 'blog author 13',
-    content: 'blog content 13'
+    resume: 'resume 13',
+    author: 'author 13',
+    content: 'content 13'
   },
   {
     title: 'blog title 14',
     publish: '2021-01-14',
-    resume: 'blog resume 14',
-    author: 'blog author 14',
-    content: 'blog content 14'
+    resume: 'resume 14',
+    author: 'author 14',
+    content: 'content 14'
   },
   {
     title: 'blog title 15',
     publish: '2021-01-15',
-    resume: 'blog resume 15',
-    author: 'blog author 15',
-    content: 'blog content 15'
+    resume: 'resume 15',
+    author: 'author 15',
+    content: 'content 15'
   },
   {
     title: 'blog title 16',
     publish: '2021-01-16',
-    resume: 'blog resume 16',
-    author: 'blog author 16',
-    content: 'blog content 16'
+    resume: 'resume 16',
+    author: 'author 16',
+    content: 'content 16'
   },
   {
     title: 'blog title 17',
     publish: '2021-01-17',
-    resume: 'blog resume 17',
-    author: 'blog author 17',
-    content: 'blog content 17'
+    resume: 'resume 17',
+    author: 'author 17',
+    content: 'content 17'
   },
   {
     title: 'blog title 18',
     publish: '2021-01-18',
-    resume: 'blog resume 18',
-    author: 'blog author 18',
-    content: 'blog content 18'
+    resume: 'resume 18',
+    author: 'author 18',
+    content: 'content 18'
   },
   {
     title: 'blog title 19',
     publish: '2021-01-19',
-    resume: 'blog resume 19',
-    author: 'blog author 19',
-    content: 'blog content 19'
+    resume: 'resume 19',
+    author: 'author 19',
+    content: 'content 19'
   },
   {
     title: 'blog title 20',
     publish: '2021-01-20',
-    resume: 'blog resume 20',
-    author: 'blog author 20',
-    content: 'blog content 20'
+    resume: 'resume 20',
+    author: 'author 20',
+    content: 'content 20'
   }
 ]
